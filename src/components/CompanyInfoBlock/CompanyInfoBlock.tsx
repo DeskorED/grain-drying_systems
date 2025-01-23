075249b7fd813fa45dd07d61650d622bf70cf44c
DeskorED <rfpfywtdf29@gmail.com>
2025-01-23 12:21:25 -0500

    add new components CurrentDryerPage.tsx
    change CatalogueItem.tsx, Catalogue.tsx, MainPage.tsx

--- src/components/CompanyInfoBlock/CompanyInfoBlock.tsx
+++ src/components/CompanyInfoBlock/CompanyInfoBlock.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 
-export const CompanyInfoBlock =  () => {
+import "./style.scss"
+
+export const CompanyInfoBlock = () => {
     return (
         <div className="company-info-block__container">
             <div className="company-info-block">
+                <div className="company-info-block__header">
 
+                </div>
+                <div className="company-info-block__text">
+
+                </div>
             </div>
         </div>
     )
